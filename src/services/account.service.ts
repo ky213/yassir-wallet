c0dd39a82c29f37973a01b70e28c725cda92b9fc
azouaoui <azouaoui.med@gmail.com>
2019-09-19 13:11:02 +0100

    fix typescript errors

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,23 +1,9 @@
-import { InsertResult, UpdateResult, DeleteResult } from 'typeorm';
-import Account, { CreateAccount } from '../models/account.model';
-import Balance from '../models/balance.model';
-import PaymentMethod from '../models/paymentMethod.model';
+import { InsertResult } from 'typeorm';
+import Account from '../models/account.model';
 import AccountDTO from '../apiv1/dtos/AccountDTO';
 
-export const create = (AccountDTO: AccountDTO): Promise<InsertResult> => {
+export const create = (accountDTO: AccountDTO): Promise<InsertResult> => {
   const account: Account = new Account();
-  account.userId = AccountDTO.userID;
-  return Account.insert(account);
-};
-
-export const update = (AccountDTO: AccountDTO): Promise<InsertResult> => {
-  const account: Account = new Account();
-  account.userId = AccountDTO.userID;
-  return Account.insert(account);
-};
-
-export const isCreated = (AccountDTO: AccountDTO): Promise<InsertResult> => {
-  const account: Account = new Account();
-  account.userId = AccountDTO.userID;
+  account.userId = accountDTO.userID;
   return Account.insert(account);
 };
